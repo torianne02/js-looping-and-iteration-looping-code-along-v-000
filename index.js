07195ed68b8625159c9ec16636b859719cc9a4ab
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-18 22:26:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ function printBadges(names) {
   }
   return names;
 }
+
+function tailsNeverFails() {
+  let tails = 0
+
+  while (Math.random() >= 0.5) {
+    tails++;
+  }
+  return `You got ${tails} tails in a row!`;
+}
